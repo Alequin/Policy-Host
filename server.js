e997524b6e21656961921920f6bb9f325ab2def8
James Cox <j-cox03@hotmail.com>
2019-07-02 19:39:17 +0100

    Added simple private policy for spin the bottle

--- server.js
+++ server.js
@@ -1,5 +1,12 @@
 const express = require('express')
+const path = require('path')
 const app = express()
 const port = 3000
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+const sendPrivatePolicy = policyFile => (_, res) => {
+  return res.sendFile(path.join(`${__dirname}/private-policies/${policyFile}`))
+}
+
+app.get('/', sendPrivatePolicy('spin-the-bottle.html'))
+
+app.listen(port, () => console.log(`Policy host is running on port ${port}!`))
