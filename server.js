3ce1974bef5e790802b21687ac7a59158097b557
James Cox <j-cox03@hotmail.com>
2019-08-10 13:14:58 +0100

    policy routes now auto generated based on policy files

--- server.js
+++ server.js
@@ -1,18 +1,22 @@
 const express = require('express')
 const path = require('path')
+const fs = require('fs')
 const app = express()
 const port = process.env.PORT || 3000
 
+const PRIVATE_POLICY_DIR = path.join(`${__dirname}/private-policies`)
+
 const sendPrivatePolicy = policyFile => (_, res) => {
-  return res.sendFile(path.join(`${__dirname}/private-policies/${policyFile}`))
+  return res.sendFile(`${PRIVATE_POLICY_DIR}/${policyFile}`)
 }
 
-app.get('/spin-the-bottle', sendPrivatePolicy('spin-the-bottle.html'))
-app.get('/pentagram', sendPrivatePolicy('pentagram.html'))
-app.get(
-  '/ask-for-a-drink',
-  sendPrivatePolicy('buy-drinks-in-another-language-translations.html')
-)
+const policyFiles = fs.readdirSync(PRIVATE_POLICY_DIR)
+
+policyFiles.forEach(fileName => {
+  const urlToUse = '/' + fileName.replace('.html', '')
+  app.get(urlToUse, sendPrivatePolicy(fileName))
+})
+
 app.get('/', (_, res) => {
   res.sendFile(path.join(`${__dirname}/home.html`))
 })
